d7cf21560f2840bc54e5f2a1fbe57da88fb8dd54
Pedro Gomes <pedro.armando15@hotmail.com>
2024-06-21 17:56:07 -0300

    feat: Setup inicial do projeto Spring v: 2.7.2 Java v: 11

---
+++ src/main/java/com/pedrogomes/teste_go2win_backend/TesteGo2winBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pedrogomes.teste_go2win_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteGo2winBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteGo2winBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedrogomes/teste_go2win_backend/TesteGo2winBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedrogomes.teste_go2win_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteGo2winBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
